781b1cb10c582fbe6261852af0ed1136ecf9ba26
suwenlong <958089904@qq.com>
2023-02-14 20:14:15 +0800

    feat:基本配置

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
+const CracoLessPlugin = require('craco-less')
 const resolve = (dir) => path.resolve(__dirname, dir)
 module.exports = {
+  plugins: [{ plugin: CracoLessPlugin }],
   webpack: {
     alias: {
       '@': resolve('src')
--- src/App.tsx
+++ src/App.tsx
@@ -3,5 +3,4 @@ import React from 'react'
 function App() {
   return <div className="App">App</div>
 }
-
 export default App
