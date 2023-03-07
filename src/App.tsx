ac5ebca9454a8538c372f505cc323ceb43420739
suwenlong <958089904@qq.com>
2023-03-07 19:46:38 +0800

    feat:懒加载组件分包

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
-import { useRoutes } from 'react-router-dom'
+import React, { Suspense } from 'react'
+import { useRoutes, Link } from 'react-router-dom'
 import route from '@/router'
 function App() {
-  return <div className="App">{useRoutes(route)}</div>
+  return (
+    <div className="App">
+      <div className="header">
+        <Link to="/discover">发现</Link>
+        <Link to="/focus">关注</Link>
+        <Link to="/mine">我的</Link>
+        <Link to="/download">下载客户端</Link>
+      </div>
+      <Suspense fallback="">
+        <div className="main">{useRoutes(route)}</div>
+      </Suspense>
+    </div>
+  )
 }
 export default App
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { lazy } from 'react'
 import { Navigate } from 'react-router-dom'
 import type { RouteObject } from 'react-router-dom'
-import Discover from '@/views/discover'
-import Mine from '@/views/mine'
-import Focus from '@/views/focus'
+const Discover = lazy(() => import('@/views/discover'))
+const Download = lazy(() => import('@/views/download'))
+const Focus = lazy(() => import('@/views/focus'))
+const Mine = lazy(() => import('@/views/mine'))
 const routes: RouteObject[] = [
   {
     path: '/',
@@ -13,6 +14,10 @@ const routes: RouteObject[] = [
   {
     path: '/discover',
     element: <Discover />
+  },
+  {
+    path: '/download',
+    element: <Download />
   },
   {
     path: '/mine',
