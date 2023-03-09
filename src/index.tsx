13e6a9df37ca10ac85a273abbed647221808750a
suwenlong <958089904@qq.com>
2023-03-09 20:16:06 +0800

    feat:redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'normalize.css'
 import './assets/css/index.less'
+// 提供注入器注入store
+import { Provider } from 'react-redux'
+import store from './store'
 import App from '@/App'
 import { HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>
 )
