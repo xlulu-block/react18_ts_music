d134b8559ebf795095afdea607b6e8170e0eb737
suwenlong <958089904@qq.com>
2023-03-23 20:15:44 +0800

    feat:轮播图基本功能实现

--- src/views/discover/c-pages/recommend/c-cpns/swiper/index.tsx
+++ src/views/discover/c-pages/recommend/c-cpns/swiper/index.tsx
@@ -1,27 +1,44 @@
-import React, { FC, memo } from 'react'
-import type { ReactNode } from 'react'
+import React, { FC, memo, useRef } from 'react'
+import type { ReactNode, ElementRef } from 'react'
 import { SwiperWrapper } from './style'
-import { Carousel } from 'antd'
+import { Carousel, Button } from 'antd'
+import { shallowEqualApp, useAppSelector } from '@/store'
 interface IProps {
   children?: ReactNode
 }
+
 const Swiper: FC<IProps> = () => {
+  // 绑定banner的ref
+  // 将组件的类型传入ref
+  const bannerRef = useRef<ElementRef<typeof Carousel>>(null)
+  // 从store中获取数据
+  const { banners } = useAppSelector(
+    (state) => ({
+      banners: state.recommend.banners
+    }),
+    shallowEqualApp
+  )
+  const handlePreview = () => {
+    console.log('上一页')
+    bannerRef.current?.prev()
+  }
+  const handleNext = () => {
+    console.log('下一页')
+    bannerRef.current?.next()
+  }
   return (
     <SwiperWrapper>
-      <Carousel autoplay>
-        <div>
-          <h3 className="contentStyle">1</h3>
-        </div>
-        <div>
-          <h3 className="contentStyle">2</h3>
-        </div>
-        <div>
-          <h3 className="contentStyle">3</h3>
-        </div>
-        <div>
-          <h3 className="contentStyle">4</h3>
-        </div>
+      <Carousel autoplay ref={bannerRef}>
+        {banners.map((item, index) => {
+          return (
+            <div className="contentStyle" key={index}>
+              <img src={item.imageUrl} alt="" />
+            </div>
+          )
+        })}
       </Carousel>
+      <Button onClick={handlePreview}>上一页</Button>
+      <Button onClick={handleNext}>下一页</Button>
     </SwiperWrapper>
   )
 }
--- src/views/discover/c-pages/recommend/c-cpns/swiper/style.ts
+++ src/views/discover/c-pages/recommend/c-cpns/swiper/style.ts
@@ -1,13 +1,10 @@
 import styled from 'styled-components'
 export const SwiperWrapper = styled.div`
-  /* height: 285px;
-  width: 100%;
-  overflow: hidden; */
   .contentStyle {
-    height: '160px';
-    color: '#fff';
-    line-height: '160px';
-    text-align: 'center';
-    background: '#364d79';
+    height: 160px;
+    color: #fff;
+    line-height: 160px;
+    text-align: center;
+    background: #364d79;
   }
 `
